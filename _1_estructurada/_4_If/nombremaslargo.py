036347d0e450cc9af3416caeff6c2c927ebfd443
MSC Tomás González <tomas.gonzalez@smartin.tecnm.mx>
2025-03-09 07:44:09 -0600

    Cambios en nombre mas largo

--- _1_estructurada/_4_If/nombremaslargo.py
+++ _1_estructurada/_4_If/nombremaslargo.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
     t2=len(n2)
 
     if t1==t2:
-        print(f"los nombres {n1} y {n2} son iguales")
+        print(f"los nombres {n1} y {n2} tienen el mismo tamaño")
     elif t1>t2:
         print("El nombre mas largo es ", n1)
     else:
-        print("El nombre mas largo es ", n2)
+        print("El nombre mas largo es "+ n2)
 
